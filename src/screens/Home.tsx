5a5e098b964645c6390fb9c72b24abe165025428
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2024-05-03 11:17:28 -0300

    feat: add home screen

---
+++ src/screens/Home.tsx
@@ -0,0 +1,77 @@
+import Header from "components/Header"
+import Table from "components/Table"
+import { useEffect, useMemo, useState } from "react";
+import { api } from "services/api";
+
+interface Client {
+  TECL_ID: number;
+  TECL_NOME: string;
+  TECL_ENDERECO: string;
+  TECL_CIDADE: string;
+  TECL_UF: string;
+  TECL_TELEFONE: string
+}
+
+function Home() {
+  const [data, setData]= useState<Client[]>([])
+
+  const handleGenerateToken = async () => {
+    try {
+      const response = await api.post('/generate/token');
+      const { token } = response.data;
+
+      localStorage.setItem('token', token);
+
+      return true;
+    } catch (error) {
+      console.error('Erro ao fazer gerar token:', error);
+      return false;
+    }
+  };
+
+  const handleGetAllClients = async () => {
+    try {
+      const response = await api.get('/teste/clientes');
+      const { data } = response.data;
+
+      console.log(data)
+      setData(data)
+
+    }  catch (error) {
+      console.error('Erro ao buscar clientes: ', error);
+      return false;
+    }
+  }
+
+  useMemo(async () => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      handleGetAllClients()
+    } else {
+      await handleGenerateToken()
+      await handleGetAllClients()
+    }
+  }, [])
+
+  return (
+   <>
+      <Header
+        title="Titulo"
+      />
+
+      <main
+        className="w-full flex justify-center"
+      >
+        <div
+          className="w-[80%]"
+        >
+          <Table
+            data={data}
+          />
+        </div>
+      </main>
+   </>
+  )
+}
+
+export default Home
