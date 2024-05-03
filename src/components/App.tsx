ccb3c3cf9f7829ea63a2de236a37fdc2d0ad828d
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2024-05-03 11:16:46 -0300

    feat: add react-router-dom

---
+++ src/components/App.tsx
@@ -0,0 +1,28 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Create from "screens/Create";
+import Home from "screens/Home";
+import Update from "screens/Update";
+
+function App() {
+  return (
+    <Router>
+        <Switch>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/update/:id">
+            <Update />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+    </Router>
+  )
+}
+
+export default App
